d877c1004840839a949af3d58209bf3f46d10b82
lookmygit <spbtvtower@gmail.com>
2016-06-13 19:19:10 +0400

    Almost Ready First Page

--- source/js/app.js
+++ source/js/app.js
@@ -1,8 +1,24 @@
-(function() {
-  'use strict';
+'use strict';
 
-  setTimeout(function() {
-    document.querySelector("#myCard").classList.toggle("flip")
-  }, 1000);
-})();
+ $(document).ready(function () {
 
+
+
+      $('.button').on('click', function (e) {
+          $('#fl').toggleClass('flip');
+          $('.button').css({'opacity':0,'visibility':'hidden'});
+      });
+      $(document).on('click', function (e) {
+          if ($(e.target).is('#good,#form,#sub,radio-1,radio-2,check, .button') === false) {
+              $('#fl').removeClass('flip');
+              $('.button').css({'opacity':1,'visibility':'visible'});
+          }
+      });
+
+  });
+
+ $(document).ready(function () {
+var scene = document.getElementById('scene');
+var parallax = new Parallax(scene);
+
+});
